35c5ecbc65b9f45e5796d74359d9f49c995b59ff
bestylotus <wellmoon0@163.com>
2026-04-17 11:05:15 +0800

    feat: 终端调整

--- config/launch.lua
+++ config/launch.lua
@@ -10,32 +10,19 @@ if platform.is_win then
   options.launch_menu = {
     { label = " PowerShell v1", args = { "powershell" } },
     { label = " PowerShell v7", args = { "pwsh" } },
-    { label = " Cmd", args = { "cmd" } },
-    { label = " Nushell", args = { "nu" } },
-    {
-      label = " GitBash",
-      args = { "C:\\soft\\Git\\bin\\bash.exe" },
-    },
-    {
-      label = " AlmaLinux",
-      args = { "ssh", "kali@192.168.44.147", "-p", "22" },
-    },
+    { label = "Cmd", args = { "cmd" } },
   }
 elseif platform.is_mac then
-  options.default_prog = { "/opt/homebrew/bin/fish", "--login" }
+  options.default_prog = { "bash", "--login" }
   options.launch_menu = {
-    { label = " Bash", args = { "bash", "--login" } },
-    { label = " Fish", args = { "/opt/homebrew/bin/fish", "--login" } },
-    { label = " Nushell", args = { "/opt/homebrew/bin/nu", "--login" } },
-    { label = " Zsh", args = { "zsh", "--login" } },
+    { label = "Bash", args = { "bash", "--login" } },
+    { label = "Zsh", args = { "zsh", "--login" } },
   }
 elseif platform.is_linux then
   options.default_prog = { "bash", "--login" }
   options.launch_menu = {
-    { label = " Bash", args = { "bash", "--login" } },
-    { label = " Fish", args = { "/opt/homebrew/bin/fish", "--login" } },
-    { label = " Nushell", args = { "/opt/homebrew/bin/nu", "--login" } },
-    { label = " Zsh", args = { "zsh", "--login" } },
+    { label = "Bash", args = { "bash", "--login" } },
+    { label = "Zsh", args = { "zsh", "--login" } },
   }
 end
 
